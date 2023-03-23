181bcee2722ae970bdab9ea063f1f3e7133faa1b
Shubhanshu Paliwal <61360632+shubhanshu2002@users.noreply.github.com>
2023-03-23 20:08:20 +0530

    Add files via upload

---
+++ Asset/js/index.js
@@ -0,0 +1,32 @@
+﻿let flag = 0;
+
+function controller(x) {
+
+    flag = flag + x;
+    slideShow(flag);
+    console.log(flag);
+}
+
+slideShow(flag);
+
+function slideShow(num) {
+
+    let totalSlides = document.querySelectorAll(" .slides");
+    console.log(totalSlides);
+
+    if (num == totalSlides.length) {
+        num = 0;
+        flag = 0;
+    }
+    if (num < 0) {
+        num = totalSlides.length - 1;
+        flag = totalSlides.length - 1;
+    }
+
+
+
+    for (let i = 0; i < totalSlides.length; i++) {
+        totalSlides[i].style.display = "none";
+    }
+    totalSlides[num].style.display = "block";
+}
